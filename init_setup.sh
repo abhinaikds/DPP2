8930a9eae9f7be43fb57f69f4f6608b0d73c158d
“Abhishek <abhinaikds@gmail.com>
2024-02-07 13:34:26 +0530

    edited

--- init_setup.sh
+++ init_setup.sh
@@ -4,7 +4,7 @@ echo [$(date)]: "START"
 echo [$(date)]: "creating env with python 3.9 version" 
 
 
-conda create --prefix ./env python=3.9 -y
+conda create --prefix ./env python=3.9 -y   
 
 
 echo [$(date)]: "activating the environment" 
